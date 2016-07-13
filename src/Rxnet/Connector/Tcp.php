9786d2af0eed7425455c925166f26cc01675fba5
Vincent Vermersch <Vinceveve@users.noreply.github.com>
2016-07-13 10:49:25 +0200

    Update Tcp.php

--- src/Rxnet/Connector/Tcp.php
+++ src/Rxnet/Connector/Tcp.php
@@ -2,6 +2,8 @@
 namespace Rxnet\Connector;
 
 use React\Socket\ConnectionException;
+use Rx\Observable;
+use Rx\ObserverInterface;
 use Rxnet\Event\ConnectorEvent;
 use Rxnet\Transport\Stream;
 
@@ -13,31 +15,35 @@ class Tcp extends Connector
     protected $protocol = "tcp";
 
     /**
-     * @return resource
+     * @return Observable\AnonymousObservable
+     * @throws \Exception
      */
     protected function createSocketForAddress()
     {
         $socket = parent::createSocketForAddress();
 
         // Wait TCP handshake
-        $this->loop->addWriteStream($socket, [$this, 'onConnected']);
-
-        return $socket;
+        return Observable::create(function(ObserverInterface $observer) use($socket) {
+            $this->loop->addWriteStream($socket, function($socket) use($observer) {
+                $this->onConnected($socket, $observer);
+            });
+        });
     }
 
     /**
      * @param $socket
+     * @param ObserverInterface $observer
      */
-    public function onConnected($socket)
+    public function onConnected($socket, $observer)
     {
         $this->loop->removeWriteStream($socket);
         if (false === stream_socket_get_name($socket, true)) {
-            $this->notifyError(new ConnectionException('Connection refused'));
-            $this->notifyCompleted();
+            $observer->onError(new ConnectionException('Connection refused'));
+            $observer->onCompleted();
             return;
         }
-        $this->notifyNext(new ConnectorEvent("/connector/connected", new Stream($socket, $this->loop), $this->labels));
-        $this->notifyCompleted();
+        $observer->onNext(new ConnectorEvent("/connector/connected", new Stream($socket, $this->loop), $this->labels));
+        $observer->onCompleted();
     }
 
 }
