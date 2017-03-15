a64e1019b546c90508858f5b8dedb369d0084c3d
etienne <er@4x.fr>
2017-03-15 11:00:18 +0100

    add one more spec for http chunk parser

--- specs/Http/http-request.spec.php
+++ specs/Http/http-request.spec.php
@@ -1,6 +1,7 @@
 <?php
 /*
  * request 1 : several chunks in 2 packets, packets cut in middle of a chunk
+ * request 2 : several chunks in 1 packet
  */
 describe("RX Http request parser", function () {
 
@@ -16,6 +17,28 @@ describe("RX Http request parser", function () {
                 new \Rxnet\Event\Event('/packet', ""),
                 new \Rxnet\Event\Event('/packet', ""),
                 new \Rxnet\Event\Event('/packet', $p2),
+            ];
+
+            $observable = \Rx\Observable::fromArray($packets);
+
+            $request = new \Rxnet\Http\HttpRequest(new \GuzzleHttp\Psr7\Request("GET", "/"));
+
+            $request->subscribeCallback(function (\GuzzleHttp\Psr7\Response $response) use ($r) {
+                $responseBody = $response->getBody()->getContents();
+
+                expect($responseBody)->to->equal($r);
+            });
+
+            $observable->subscribe($request);
+
+        });
+        it("Parse Multiple chunks in one packet", function () {
+            $p = $this->loadFixture('rawtext:http:request2');
+            $r = $this->loadFixture('rawtext:http:response2');
+
+
+            $packets = [
+                new \Rxnet\Event\Event('/packet', $p),
             ];
 
             $observable = \Rx\Observable::fromArray($packets);
