dabbde6bf7fa5e2e88efd385f439aef436817010
etienne <er@4x.fr>
2017-04-04 17:03:57 +0200

    upgrade HasLabelsInterface

--- src/Rxnet/Contract/EventInterface.php
+++ src/Rxnet/Contract/EventInterface.php
@@ -2,31 +2,12 @@
 namespace Rxnet\Contract;
 
 
-interface EventInterface
+interface EventInterface extends HasLabelsInterface
 {
     public function getName();
-    public function getLabels();
     public function setName($name);
     public function getData($key = null);
     public function setData($data);
     public function is($name);
     public function isLike($pattern);
-    /**
-     * @param string $name filter with * possible
-     * @return bool
-     */
-
-    /**
-     * @param $key
-     * @param $value
-     * @return bool
-     */
-    public function hasLabel($key, $value = null);
-
-    /**
-     * @param $key
-     * @return mixed
-     */
-    public function getLabel($key = null);
-
 }
--- src/Rxnet/Contract/HasLabelsInterface.php
+++ src/Rxnet/Contract/HasLabelsInterface.php
@@ -8,4 +8,19 @@ interface HasLabelsInterface
      * @return array
      */
     public function getLabels();
+
+    /**
+     * @param $key
+     * @param $value
+     * @return bool
+     */
+    public function hasLabel($key, $value = null);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getLabel($key = null);
+
+    public function addLabel($key, $value);
 }
