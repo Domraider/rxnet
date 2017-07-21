5669f01353c77fc4c7613328e5b3660a92874d65
etienne <er@4x.fr>
2017-07-21 14:29:18 +0200

    manges http responses paclet-splited in headers

--- specs/Http/http-request.spec.php
+++ specs/Http/http-request.spec.php
@@ -5,6 +5,43 @@
  */
 describe("RX Http request parser", function () {
 
+    beforeEach(function () {
+        $this->specDone = new \Rx\Subject\Subject();
+    });
+    afterEach(function () {
+        \Rxnet\await($this->specDone->timeout(1000));
+    });
+
+    context("Multiple packets", function () {
+        it("Parse response in multiple packets, splited in the headers", function () {
+            $p1 = $this->loadFixture('rawtext:http:request3-packet1');
+            $p2 = $this->loadFixture('rawtext:http:request3-packet2');
+            $r = $this->loadFixture('rawtext:http:response3');
+
+
+            $packets = [
+                new \Rxnet\Event\Event('/packet', $p1),
+                new \Rxnet\Event\Event('/packet', $p2),
+            ];
+
+            $observable = \Rx\Observable::fromArray($packets);
+
+            $request = new \Rxnet\Http\HttpRequest(new \GuzzleHttp\Psr7\Request("GET", "/"));
+
+            $request->subscribeCallback(function (\GuzzleHttp\Psr7\Response $response) use ($r) {
+                $responseBody = $response->getBody()->getContents();
+
+                expect($responseBody)->to->equal($r);
+
+                $this->specDone->onCompleted();
+            });
+
+            $observable->subscribe($request);
+
+
+        });
+    });
+
     context("Chunked", function () {
         it("Parse Multiple chunks in multiple packets", function () {
             $p1 = $this->loadFixture('rawtext:http:request1-packet1');
@@ -27,10 +64,11 @@ describe("RX Http request parser", function () {
                 $responseBody = $response->getBody()->getContents();
 
                 expect($responseBody)->to->equal($r);
+
+                $this->specDone->onCompleted();
             });
 
             $observable->subscribe($request);
-
         });
         it("Parse Multiple chunks in one packet", function () {
             $p = $this->loadFixture('rawtext:http:request2');
@@ -49,6 +87,8 @@ describe("RX Http request parser", function () {
                 $responseBody = $response->getBody()->getContents();
 
                 expect($responseBody)->to->equal($r);
+
+                $this->specDone->onCompleted();
             });
 
             $observable->subscribe($request);
